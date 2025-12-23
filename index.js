91ba8420cf59754cc5940d50d9c6ef32cf9a2591
qadrinaved <99243961+qadrinaved@users.noreply.github.com>
2025-12-23 23:29:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 const display = require("./display");
-const deleteData = require("./delete");
+const dgetData = require("./products");
 
 app.get("/", (req, res) => {
   res.send("Overview page");
 });
 
 app.get("/display", display);
-app.delete("/delete", deleteData);
+app.delete("/products", dgetData);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
